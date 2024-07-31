dd1b84aa9e1478bbe34bed0822229687386b1853
pratim2003 <duttapratim003@gmail.com>
2024-07-31 12:27:08 +0530

    cors and cookie-parser

--- src/index.js
+++ src/index.js
@@ -1,9 +1,29 @@
 import dotenv from 'dotenv'
 import dataBaseConnect from "./db/index.js";
-dataBaseConnect();
+import { app } from './app.js';
 dotenv.config({
     path : "./env"
 })
+dataBaseConnect()
+.then(()=>{
+    app.on("error",(error)=>{
+        console.log(`there an error at ${error}`)
+    })
+    app.get("/",(req,res)=>{
+        res.send("hii i am pratim")
+    })
+    app.get("/git",(req,res)=>{
+        res.send("<h2>hii this my github</h2>")
+    })
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`server is on at ${process.env.PORT}`)
+    })
+    
+})
+.catch((error)=>{
+    console.log(`unable to connect database : ${error}`)
+})
+
 /*
 const app = express()
 
